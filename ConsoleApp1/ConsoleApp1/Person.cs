031ce18af6234c356aaf4bf99a5b7e01e3d9fa52
Bekzod <ahmadovbekzod94@gmail.com>
2024-12-07 19:08:07 +0500

    Object-Oriented Programming

---
+++ ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP;
+
+public class Person(string firstname, string lastname, DateOnly birthday)
+{
+    public string Firstname { get; } = firstname;
+
+    public string Lastname { get; } = lastname;
+
+    public DateOnly Birthday { get; } = birthday;
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using OOP;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person1 = new Person("Bekzod", "Akhmadov", new DateOnly(2002, 10, 26));
+            var person2 = new Person("Asiljon", "Akmaljonov", new DateOnly(2000, 12, 30));
+
+            List<Person> peopla = [person1, person2];
+
+        }
+
+    }
+}
