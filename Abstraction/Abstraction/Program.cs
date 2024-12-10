2143848ee7008d089b61e0b2044982b9c3947b04
Bekzod <ahmadovbekzod94@gmail.com>
2024-12-10 10:58:51 +0500

    try

---
+++ Abstraction/Abstraction/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Abstraction
+{
+    internal abstract class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
